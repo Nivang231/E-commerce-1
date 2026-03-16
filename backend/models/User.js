9d81fd43d9045fc388d985a0ff46598c2ea0d2ef
Nivang Goyal <nivang231@gmail.com>
2026-03-17 00:53:01 +0530

    Backend Started

---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        match: [/.+\@.+\..+/, "Please enter a valid email address"]
+    },
+    password: {
+        type: String,
+        enum: ["customer", "admin"],
+        default: "customer",
+    }
+},
+    { timestamps: true }
+);
+
+// Password Hash Middleware 
