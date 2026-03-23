c63c942ef5759ad629cbf951156b06a66d91a9dd
Nivang Goyal <nivang231@gmail.com>
2026-03-23 19:24:14 +0530

    Update color

--- frontend/src/components/Layout/Hero.jsx
+++ frontend/src/components/Layout/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import heroImg from "../../assets/rabbit-hero.webp"
+// import heroImg from "../../assets/rabbit-hero.webp"
+import heroImg from "../../assets/hero-image.jpg"
 import { Link } from 'react-router-dom'
 
 const Hero = () => {
